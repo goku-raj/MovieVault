70f68702bbb593deee8b792ff980e6c550cb5e70
goku-raj <anshuraj1479@gmail.com>
2023-08-19 14:11:41 +0530

    API & Redux Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import { fetchData } from './utils/api';
+import { useDispatch, useSelector } from 'react-redux';
+import { getApiConfiguration, getGenres } from './store/homeSlice';
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const dispatch = useDispatch();
+  const {url} = useSelector((state)=> state.home)
+  // console.log(url)
+  
+  useEffect(()=>{
+    apiTesting()
+  }, [])
+
+  const apiTesting = () =>{
+    fetchData('/movie/popular')
+    .then((res)=>{
+      console.log(res)
+      dispatch(getApiConfiguration(res))
+    })
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     {url?.total_pages}
     </>
   )
 }
