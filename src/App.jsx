2b13db496696499865f43a84576b4a6e6305d737
goku-raj <anshuraj1479@gmail.com>
2023-08-19 19:05:05 +0530

    Hero Banner Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import { useState, useEffect } from 'react'
 import { fetchData } from './utils/api';
 import { useDispatch, useSelector } from 'react-redux';
 import { getApiConfiguration, getGenres } from './store/homeSlice';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
 
 function App() {
 
@@ -10,20 +19,35 @@ function App() {
   // console.log(url)
   
   useEffect(()=>{
-    apiTesting()
+    fetchApiConfig()
   }, [])
 
-  const apiTesting = () =>{
-    fetchData('/movie/popular')
+  const fetchApiConfig = () =>{
+    fetchData('/configuration')
     .then((res)=>{
       console.log(res)
-      dispatch(getApiConfiguration(res))
+
+      const url ={
+        backdrop: res.images.secure_base_url + 'original',
+        poster: res.images.secure_base_url + 'original',
+        profile: res.images.secure_base_url + 'original'
+      }
+
+      dispatch(getApiConfiguration(url))
     })
   }
   return (
-    <>
-     {url?.total_pages}
-    </>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   )
 }
 
