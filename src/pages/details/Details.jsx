261e06174cdd6825b9c87cb7fdc36883d35c6db0
goku-raj <anshuraj1479@gmail.com>
2023-08-20 15:35:56 +0530

    DetailsPage TopSections

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import './style.scss';
+import React from "react";
+import "./style.scss";
+import useFetch from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
+import Cast from "./cast/Cast";
 
 const Details = () => {
-  return (
-    <div>Details</div>
-  )
-}
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+  const { data: credits, loading: creditsLoading } = useFetch(
+    `/${mediaType}/${id}/credits`
+  );
 
-export default Details
+  return (
+    <div>
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={creditsLoading}/>
+
+      <div style={{ height: "1000px" }}></div>
+    </div>
+  );
+};
+
+export default Details;
